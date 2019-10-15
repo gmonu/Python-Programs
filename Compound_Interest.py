fd88546f46d03c4a737927ca87c8bfc33f53c23c
= <=>
2019-10-15 11:41:20 +0530

    15-10-2019

---
+++ Compound_Interest.py
@@ -0,0 +1,8 @@
+  
+def compound_interest(principle, rate, time): 
+    
+    CI = principle * (pow((1 + rate / 100), time)) 
+    print("Compound interest is", CI) 
+  
+# Driver Code  
+compound_interest(10000, 10.25, 5)
